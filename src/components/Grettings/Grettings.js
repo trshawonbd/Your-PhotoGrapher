17a4fd3eba145c9fd883a6f01863886bf92293a3
Your Name <you@example.com>
2022-04-16 22:11:40 +0300

    primary setup , images and json file create

---
+++ src/components/Grettings/Grettings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Grettings.css';
+
+const Grettings = () => {
+    return (
+        <div>
+            <h2>This is Greetings Page</h2>
+        </div>
+    );
+};
+
+export default Grettings;
---
+++ src/components/Shared/SocialLogin/SocialLogin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SocialLogin.css';
+
+const SocialLogin = () => {
+    return (
+        <div>
+            <h2>This is Social Login</h2>
+        </div>
+    );
+};
+
+export default SocialLogin;
