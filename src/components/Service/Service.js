d2513bab6cfe9bb003278e2a0a5d5b6ae2e9bf48
Your Name <you@example.com>
2022-04-17 03:46:33 +0300

    services, single service done without button

---
+++ src/components/Service/Service.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import './Service.css';
+
+const Service = ({ service }) => {
+
+    const [showMore, setShowMore] = useState(false);
+    const {name, price, picture, about} = service;
+    return (
+        <div className="col service-card">
+            <div className="card h-100 card-img" style={{ width: "18rem" }}>
+                <img src={picture} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{name}</h5>
+                    <p className="card-text">
+                    {showMore ? about : `${about.slice(0,300)} `
+                        }
+                            <button className='btn-read-more' onClick={()=> setShowMore(!showMore)}>
+                            {showMore ? 'Read Less' : 'Read More'} 
+                            </button>  
+                    </p>
+                    <hr />
+                    <h4 className='card-price'>Price:{price}</h4>
+                    <hr />
+                    <button    /* onClick={() => addToCart(service)} */ className='book-now-btn ms-3'>Book Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
 import './Services.css';
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }
+        , [])
+
     return (
-        <div>
-            <h2>This is services</h2>
+        <div className='container '>
+            <h2 className='my-5'>All services</h2>
+            <div class="row row-cols-1 row-cols-md-3 g-4 ">
+
+                {
+                    services.map((service) => <Service
+                        key={service.id}
+                        service={service}
+
+                    ></Service>)
+                }
+
+            </div>
         </div>
     );
 };
