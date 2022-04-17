f226bb9dffc7be4bd45e439f0951345bebc2edf9
Your Name <you@example.com>
2022-04-17 04:52:55 +0300

    Not Found created

--- src/components/Shared/NotFound/NotFound.js
+++ src/components/Shared/NotFound/NotFound.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './NotFound.css';
+import gif from '../../../images/NotFound/giphy.gif';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>Here is NotFound</h2>
+    <div className='container'>
+        <h2 className='py-5'>Not Found</h2>
+    
+        
+        <div className='not-found w-100'>
+            
+            <img src={gif} alt="" srcset="" />      
+        </div>
         </div>
     );
 };
